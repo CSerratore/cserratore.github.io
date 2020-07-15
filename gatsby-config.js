788c1a012419b434df27fc04ff0730b164ae795c
Cory Serratore <cory.serratore@gmail.com>
2020-07-15 17:44:54 -0400

    shadowed contact-form to decode email address

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    siteMetadata: {
+        description: "Personal page of Cory Serratore",
+        title: "Cory Serratore",
+    },
+    plugins: [
+      {
+        resolve: "@wkocjan/gatsby-theme-intro",
+        options: {
+          theme: "blue",
+          showThemeLogo: false,
+        },
+      },
+    ],
+  }
