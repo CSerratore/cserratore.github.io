dd0a298df46f4d9310e4c278de560bc1fa2ef482
Cory Serratore <cory.serratore@gmail.com>
2021-04-18 09:03:03 -0400

    Updated theme and Gatsby to latest versions

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,17 @@
 module.exports = {
-    siteMetadata: {
-        description: "Personal page of Cory Serratore",
-        title: "Cory Serratore",
-    },
-    plugins: [
-      {
-        resolve: "@wkocjan/gatsby-theme-intro",
-        options: {
-          theme: "blue",
-          showThemeLogo: false,
-        },
+  siteMetadata: {
+      description: "Personal page of Cory Serratore",
+      title: "Cory Serratore",
+      locale: "en",
+      formspreeEndpoint: "https://formspree.io/f/xgervkqv"
+  },
+  plugins: [
+    {
+      resolve: "@wkocjan/gatsby-theme-intro",
+      options: {
+        theme: "blue",
+        showThemeLogo: false,
       },
-    ],
-  }
+    },
+  ],
+}
