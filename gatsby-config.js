afac7ad74d496dbb91d627a8b0d4edf85d3fc2b7
Cory Serratore <30655866+CSerratore@users.noreply.github.com>
2022-09-06 15:01:02 -0400

    removed contact form and budget

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,7 @@ module.exports = {
   siteMetadata: {
       description: "Personal page of Cory Serratore",
       title: "Cory Serratore",
-      locale: "en",
-      formspreeEndpoint: "https://formspree.io/f/xgervkqv"
+      locale: "en"
   },
   plugins: [
     {
